9e48858644b0c92ddf27e840f16a07727038f66b
Ammon Roberts <ammoneroberts@gmail.com>
2019-10-07 11:18:46 -0600

    Update all

--- Grant's Tomb/js/main.js
+++ Grant's Tomb/js/main.js
@@ -4,15 +4,16 @@ const question = {
     stem: `Who is buried in Grant's Tomb?`,
     option1: `Bob Dylan`,
     option2: `George Washington`,
-    option3: `Grant`,
-    creect: `3`,
-
+    option3: `Ulysses S. Grant`,
+    option4: `Jay Leno`,
+    correct: 3,
     display: () => {
         document.querySelector('#stem').textContent = question.stem;
         document.querySelector('#answerOne').textContent = question.option1;
         document.querySelector('#answerTwo').textContent = question.option2;
         document.querySelector('#answerThree').textContent = question.option3;
         document.querySelector('#answerFour').textContent = question.option4;
+        console.log('farts');
     },
     check: (userChoice) => {
         if (userChoice === question.correct)
@@ -24,8 +25,8 @@ const question = {
 }
 
 document.querySelector('#answerOne').addEventListener('click', () => question.check(1))
-document.querySelector('#answerOne').addEventListener('click', () => question.check(2))
-document.querySelector('#answerOne').addEventListener('click', () => question.check(3))
-document.querySelector('#answerOne').addEventListener('click', () => question.check(4))
+document.querySelector('#answerTwo').addEventListener('click', () => question.check(2))
+document.querySelector('#answerThree').addEventListener('click', () => question.check(3))
+document.querySelector('#answerFour').addEventListener('click', () => question.check(4))
 
 question.display();
