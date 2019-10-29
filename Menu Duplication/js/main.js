476646b3489e6347e3e93a0c0178f3d79a46df57
Ammon Roberts <ammoneroberts@gmail.com>
2019-10-29 17:00:42 -0600

    Update all

--- Menu Duplication/js/main.js
+++ Menu Duplication/js/main.js
@@ -1,16 +1,23 @@
 function duplicateMenu() {
+    console.log(document.querySelector('#primaryNavigation'));
+    console.log(document.querySelector('#smallNavArea'));
+
     let menuList = document.querySelectorAll('ul#primaryNavigation li a');
 
     let newMenuList = document.createElement('ul');
-    array.forEach(element => {
+
+    menuList.forEach(element => {
         let newItem = document.createElement('li');
         let newLink = document.createElement('a');
         newLink.setAttribute('href', element.getAttribute('href'));
-
+        newLink.setAttribute('value', element.innerHTML);
         //copy the textcontent from upper menu to new menu
         //append children to show at the bottom
-    });
+        newItem.appendChild(newLink);
+        document.querySelector('#smallNavArea').appendChild(newItem);
 
+    });
+    document.querySelector('#smallNavArea').display = 'block';
 }
 
 duplicateMenu();
