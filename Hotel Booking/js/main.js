1a731ef4a0fc1579ebcbfe75f0091186bda0e278
Ammon Roberts <ammoneroberts@gmail.com>
2019-11-13 14:39:13 -0700

    Update all

--- Hotel Booking/js/main.js
+++ Hotel Booking/js/main.js
@@ -1,11 +1,30 @@
 async function getHotelData() {
     try {
-        const response = await fetch('../hotel.json');
+        const response = await fetch('./hotel.json');
         return await response.json();
     } catch (error) {
         console.error(error);
     }
 
 }
+
 let hotelData = {};
 getHotelData().then(data => hotelData = data);
+
+let links = document.querySelectorAll('a');
+//use links to loop over each element and addEventListener to each one
+//Use forEach
+document.querySelector('#hotelName').textContent = '';
+document.querySelector('#marriott').addEventListener('click', hotelInfo);
+document.querySelector('#hilton').addEventListener('click', hotelInfo);
+document.querySelector('#sheriton').addEventListener('click', hotelInfo);
+
+function hotelInfo(event) {
+    let hotelChoice = hotelData.hotels.find(hotel => {
+        return event.target.id === hotel.name.toLowerCase();
+    })
+    console.log(hotelChoice);
+
+    document.querySelector('#hotelName').textContent = `${hotelChoice.name} Hotel`;
+
+}
