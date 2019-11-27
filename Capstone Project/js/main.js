1288abaff60c770d96cdd0b07af885677bcbdb8a
Ammon Roberts <ammoneroberts@gmail.com>
2019-11-27 13:31:39 -0700

    Added other button functionality

--- Capstone Project/js/main.js
+++ Capstone Project/js/main.js
@@ -1,5 +1,7 @@
 let question = '';
 let answer = '';
+let category = '';
+let category_id = 0;
 let flag = 0;
 
 async function getRandomTrivia() {
@@ -8,31 +10,63 @@ async function getRandomTrivia() {
             return response.json();
         })
         .then(function (data) {
-            //conditional stuff for categories
             question = data[0].question.charAt(0).toUpperCase() + data[0].question.slice(1);
             answer = data[0].answer.charAt(0).toUpperCase() + data[0].answer.slice(1);
+            category = data[0].category.title.charAt(0).toUpperCase() + data[0].category.title.slice(1);
+            console.log(category);
+            console.log(question);
+            console.log(answer);
             showQuestion();
             flag = 0;
-            return function () {
-                return data;
-            };
+
         })
         .catch(function (err) {
             console.log("Something went wrong!", err);
         });
+}
 
+async function getFromCategory() {
+    fetch(`http://jservice.io/api/random/?category_id=${category_id}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //conditional stuff for categories
+            question = data[0].question.charAt(0).toUpperCase() + data[0].question.slice(1);
+            answer = data[0].answer.charAt(0).toUpperCase() + data[0].answer.slice(1);
+            category = data[0].category.title.charAt(0).toUpperCase() + data[0].category.title.slice(1);
+            // console.log(category);
+            // console.log(question);
+            // console.log(answer);
+            showQuestion();
+            flag = 0;
+        })
+        .catch(function (err) {
+            console.log("Something went wrong!", err);
+        });
 }
 
 document.querySelector(`#randomQuestion`).addEventListener('click', getRandomTrivia);
 document.querySelector(`#getAnswer`).addEventListener('click', showAnswer);
+document.querySelector(`#historyLesson`).addEventListener('click', () => {
+    category_id = 14685;
+    getFromCategory();
+});
+document.querySelector(`#humanBody`).addEventListener('click', () => {
+    category_id = 710;
+    getFromCategory();
+});
 
 function showQuestion() {
+    document.querySelector('#category').innerHTML = `<b>${category}</b>`;
     document.querySelector('#output').innerHTML = question;
 }
 
 function showAnswer() {
     if (flag == 0) {
         let p = document.createElement("p");
+        p.innerHTML = question;
+        document.querySelector('#category').appendChild(p);
         p.innerHTML = answer;
         document.querySelector('#output').appendChild(p);
         flag = 1;
