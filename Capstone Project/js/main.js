0f7013f1e4539549106907fe4f4a22c168ce358c
Ammon Roberts <ammoneroberts@gmail.com>
2019-11-26 17:49:20 -0700

    update all

--- Capstone Project/js/main.js
+++ Capstone Project/js/main.js
@@ -1,10 +1,17 @@
+let question = '';
+let answer = '';
+let flag = 0;
+
 async function getRandomTrivia() {
     fetch('http://jservice.io/api/random/')
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            document.querySelector('#output').innerHTML = `${data[0].question} <br> ${data[0].answer}`;
+            question = data[0].question.charAt(0).toUpperCase() + data[0].question.slice(1);
+            answer = data[0].answer.charAt(0).toUpperCase() + data[0].answer.slice(1);
+            showQuestion();
+            flag = 0;
             return function () {
                 return data;
             };
@@ -14,4 +21,20 @@ async function getRandomTrivia() {
         });
 
 }
-getRandomTrivia();
+
+document.querySelector(`#newQuestion`).addEventListener('click', getRandomTrivia);
+document.querySelector(`#getAnswer`).addEventListener('click', showAnswer);
+
+function showQuestion() {
+    document.querySelector('#output').innerHTML = question;
+}
+
+function showAnswer() {
+    if (flag == 0) {
+        let p = document.createElement("p");
+        p.innerHTML = answer;
+        document.querySelector('#output').appendChild(p);
+        flag = 1;
+    }
+
+}
